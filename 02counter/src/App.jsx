a7af34d5f80c4560b92192a72f9ae66a87ae9812
kishan gupta <kishangupta3003@gmail.com>
2025-06-06 22:09:38 +0530

    counter project for counting number of clicks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+   // we can use let or const and for setcounter we can user any naming convention
+  let [counter, setCounter] = useState(0)
+  
+  // let counter = 0 
+
+  const addValue = () => {
+    // counter = counter + 1
+    if(counter < 20){
+    setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter-1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value : {counter}</h2>
+      <button 
+      onClick={addValue}>Add Value {counter}</button>
+      <br/>
+      <button
+      onClick={removeValue}>Remove Value {counter}</button>
+      <p>footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
