0c95b79f49ae7807f041c30508ab7643171b0cff
kishan gupta <kishangupta3003@gmail.com>
2025-06-06 22:12:22 +0530

    small change

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <>
-      <h1>Chai aur react</h1>
+      <h1>Counter by kishan </h1>
       <h2>Counter value : {counter}</h2>
       <button 
       onClick={addValue}>Add Value {counter}</button>
