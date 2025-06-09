ad4ee80afeb6b69c543d9c46e256ec4310528860
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-06-09 11:35:10 +0000

    mini context for learning context api

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const UserContext = React.createContext()
+
+
+export default UserContext;
+
+
+
+
+
+
+
+
+
+
+
+
+// the component direclty can take the access of all state  
+{/* <UserContext>
+    <Login/>
+    <Card>
+        <Data/>
+    </Card>
+
+</UserContext> */}
